5919ba28d1f935b3bdc651ea175e4e2dae8a6d52
oilprint <irynakozlova.k@gmail.com>
2023-03-13 22:12:21 -0600

    add review

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,13 @@
 $(function () {
   var mixer = mixitup('.products__grid');
+
+  $('.review__slider').slick({
+    dots: true,
+    arrows: true,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="imades/decor/prev-arrow.svg" alt="Предыдущий"></button>',
+    nextArrow: '<button type="button" class="slick-"><img src="../imades/decor/next-arrow.svg" alt="Следующий"></button>'
+
+  });
   
-});
+});next
